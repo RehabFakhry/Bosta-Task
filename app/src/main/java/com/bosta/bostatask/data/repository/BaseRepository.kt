1e6ab5722b62d3eab209c2b58570839610b16ed1
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-29 02:30:26 +0200

    create BaseRepository

---
+++ app/src/main/java/com/bosta/bostatask/data/repository/BaseRepository.kt
@@ -0,0 +1,26 @@
+package com.bosta.bostatask.data.repository
+
+import android.content.res.Resources.NotFoundException
+import android.util.Log
+import retrofit2.Response
+
+abstract class BaseRepository {
+
+    protected suspend fun <T> wrap(function: suspend() -> Response<T>): T {
+        try {
+            val response = function()
+            return if (response.isSuccessful) {
+                response.body() ?: throw NotFoundException()
+            } else {
+                Log.d("Tag", "Repository failed")
+                when (response.code()) {
+                    404 -> throw NotFoundException()
+                    else -> throw Exception(response.toString())
+                }
+            }
+        } catch (e: Exception){
+            Log.d("TAG", "Response failed: ${e.message}")
+            throw NotFoundException()
+        }
+    }
+}
