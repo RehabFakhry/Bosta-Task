f05612a6a4644b7fc99ebc5d97f53abecd3d7a47
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-29 02:42:04 +0200

    implement functions in UserRepositoryImpl

--- app/src/main/java/com/bosta/bostatask/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/bosta/bostatask/data/repository/UserRepositoryImpl.kt
@@ -1,5 +1,8 @@
 package com.bosta.bostatask.data.repository
 
+import com.bosta.bostatask.data.remote.mapper.toUserAlbumPhoto
+import com.bosta.bostatask.data.remote.mapper.toUserAlbums
+import com.bosta.bostatask.data.remote.mapper.toUserInfo
 import com.bosta.bostatask.data.remote.network.ApiService
 import com.bosta.bostatask.domain.model.UserAlbumPhoto
 import com.bosta.bostatask.domain.model.UserAlbums
@@ -10,15 +13,15 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(
     private val apiService: ApiService
 ): BaseRepository(), UserRepository {
-    override suspend fun getAllUsers(): List<UserInfo>? {
-        TODO("Not yet implemented")
+    override suspend fun getAllUsers(): List<UserInfo> {
+        return (listOf(wrap { apiService.getAllUsers() } .toUserInfo()))
     }
 
-    override suspend fun getAllAlbumsForUser(userId: Int): List<UserAlbums>? {
-        TODO("Not yet implemented")
+    override suspend fun getAllAlbumsForUser(userId: Int): List<UserAlbums> {
+        return (listOf(wrap { apiService.getAllAlbumsForUser(userId) }.toUserAlbums()))
     }
 
-    override suspend fun getAllPhotosForAlbum(albumId: Int): List<UserAlbumPhoto>? {
-        TODO("Not yet implemented")
+    override suspend fun getAllPhotosForAlbum(albumId: Int): List<UserAlbumPhoto> {
+        return (listOf(wrap { apiService.getAllPhotosForAlbum(albumId) }.toUserAlbumPhoto()))
     }
 }
