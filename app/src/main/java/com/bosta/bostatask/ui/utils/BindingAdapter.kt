bbd13bfc88f1f5ea27c2b936460c3c099dc30be0
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-30 22:19:31 +0200

    add function to loadImage from api service

--- app/src/main/java/com/bosta/bostatask/ui/utils/BindingAdapter.kt
+++ app/src/main/java/com/bosta/bostatask/ui/utils/BindingAdapter.kt
@@ -1,10 +1,23 @@
 package com.bosta.bostatask.ui.utils
 
+import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import androidx.recyclerview.widget.RecyclerView
+import com.bosta.bostatask.R
 import com.bosta.bostatask.ui.base.BaseAdapter
+import com.bumptech.glide.Glide
 
 @BindingAdapter(value = ["app:items"])
 fun <T> setRecyclerItems(view: RecyclerView, items: List<T>?) {
     (view.adapter as BaseAdapter<T>?)?.setItems(items ?: emptyList())
+}
+
+@BindingAdapter("app:loadImage")
+fun bindImage(image: ImageView, imageURL: String?) {
+    imageURL?.let {
+        Glide.with(image)
+            .load(imageURL)
+            .placeholder(R.drawable.image_placeholder)
+            .into(image)
+    }
 }
