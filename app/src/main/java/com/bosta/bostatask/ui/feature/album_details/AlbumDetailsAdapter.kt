e437a4d011a510604fdacb4dcaca7767ddca713f
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-30 15:29:34 +0200

    create album detailsAdapter

---
+++ app/src/main/java/com/bosta/bostatask/ui/feature/album_details/AlbumDetailsAdapter.kt
@@ -0,0 +1,12 @@
+package com.bosta.bostatask.ui.feature.album_details
+
+import com.bosta.bostatask.R
+import com.bosta.bostatask.ui.base.BaseAdapter
+import com.bosta.bostatask.ui.base.BaseInteractionListener
+
+class AlbumDetailsAdapter(listener: AlbumDetailsInteractionListener)
+    : BaseAdapter<AlbumDetailsUiState>(listener) {
+    override val layoutId: Int = R.layout.item_album_details
+}
+
+interface AlbumDetailsInteractionListener: BaseInteractionListener
