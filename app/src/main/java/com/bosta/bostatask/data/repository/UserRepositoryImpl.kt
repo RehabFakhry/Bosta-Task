999d46948f85443b3d07277e5539a0d4cec5e536
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-30 10:01:28 +0200

    fix Repository to apply mapping on data

--- app/src/main/java/com/bosta/bostatask/data/repository/UserRepositoryImpl.kt
+++ app/src/main/java/com/bosta/bostatask/data/repository/UserRepositoryImpl.kt
@@ -1,9 +1,9 @@
 package com.bosta.bostatask.data.repository
 
-import com.bosta.bostatask.data.remote.mapper.toUserAlbumPhoto
-import com.bosta.bostatask.data.remote.mapper.toUserAlbums
-import com.bosta.bostatask.data.remote.mapper.toUserInfo
-import com.bosta.bostatask.data.remote.network.ApiService
+import com.bosta.bostatask.data.source.remote.mapper.toUserAlbumPhoto
+import com.bosta.bostatask.data.source.remote.mapper.toUserAlbums
+import com.bosta.bostatask.data.source.remote.mapper.toUserInfo
+import com.bosta.bostatask.data.source.remote.network.ApiService
 import com.bosta.bostatask.domain.model.UserAlbumPhoto
 import com.bosta.bostatask.domain.model.UserAlbums
 import com.bosta.bostatask.domain.model.UserInfo
@@ -13,15 +13,15 @@ import javax.inject.Inject
 class UserRepositoryImpl @Inject constructor(
     private val apiService: ApiService
 ): BaseRepository(), UserRepository {
-    override suspend fun getAllUsers(): List<UserInfo> {
-        return (listOf(wrap { apiService.getAllUsers() } .toUserInfo()))
+    override suspend fun getUserInfo(): List<UserInfo> {
+        return wrap { apiService.getUserInfo()}.map{it.toUserInfo()}
     }
 
     override suspend fun getAllAlbumsForUser(userId: Int): List<UserAlbums> {
-        return (listOf(wrap { apiService.getAllAlbumsForUser(userId) }.toUserAlbums()))
+        return wrap { apiService.getAllAlbumsForUser(userId) }.map { it.toUserAlbums() }
     }
 
     override suspend fun getAllPhotosForAlbum(albumId: Int): List<UserAlbumPhoto> {
-        return (listOf(wrap { apiService.getAllPhotosForAlbum(albumId) }.toUserAlbumPhoto()))
+        return wrap { apiService.getAllPhotosForAlbum(albumId) }.map { it.toUserAlbumPhoto() }
     }
 }
