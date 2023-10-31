5dfa175d22fb6099815dc9cae9d7dcfaeac52361
Rehab Fakhry <rehab.fakhry20@gmail.com>
2023-10-31 03:00:27 +0200

    enhance get random userIds and randomAlbum for the same user

--- app/src/main/java/com/bosta/bostatask/domain/usecase/GetUserAlbumsUseCase.kt
+++ app/src/main/java/com/bosta/bostatask/domain/usecase/GetUserAlbumsUseCase.kt
@@ -3,11 +3,13 @@ package com.bosta.bostatask.domain.usecase
 import com.bosta.bostatask.domain.model.UserAlbums
 import com.bosta.bostatask.domain.repository.UserRepository
 import javax.inject.Inject
+import kotlin.random.Random
 
 class GetUserAlbumsUseCase @Inject constructor(
     private val userRepository: UserRepository
 ) {
-    suspend operator fun invoke(userId: Int): List<UserAlbums>{
-        return userRepository.getAllAlbumsForUser(userId)?: emptyList()
+    suspend operator fun invoke(): List<UserAlbums>{
+        val randomUserId = Random.nextInt(1, 11)
+        return userRepository.getAllAlbumsForUser(randomUserId)?: emptyList()
     }
 }
